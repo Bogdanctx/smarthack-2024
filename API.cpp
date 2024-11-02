5e555fb6ef0f91203a73559b4ebb4e1b99cbc128
bgd <trifan.bogdan09@yahoo.com>
2024-11-02 16:27:38 +0200

    doamne ajuta

--- API.cpp
+++ API.cpp
@@ -6,27 +6,57 @@
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
 #include <iostream>
-#include <unordered_map>
 
-API::API()
-{
-
-}
-/*
-void API::startSession(std::unord)
+void API::startSession(std::unordered_map<std::string, std::vector<std::string>> data)
 {
     cpr::Url link{"http://localhost:8080/api/v1/session/start"};
     cpr::Header header = {
         {"API-KEY", "7bcd6334-bc2e-4cbf-b9d4-61cb9e868869"},
     };
 
+    nlohmann::json json = {
+        {""}
+    };
+
     cpr::Response res = cpr::Post(link, header);
+}
 
-    nlohmann::json json = nlohmann::json::parse(res.text);
 
-    for(auto it: json)
+void API::playRound()
+{
+    nlohmann::json j;
+
+    j["day"] = "12";
+    j["movements"] = nlohmann::json::array();
+
+    j["movements"].push_back({
+        {"connectionId", "3fa85f64-5717-4562-b3fc-2c963f66afa6"},
+        {"amount", "0"}
+    });
+
+    cpr::Response res = cpr::Post(
+        cpr::Url{"http://localhost:8080/api/v1/play/round"},
+        cpr::Header{
+            {"API-KEY", "7bcd6334-bc2e-4cbf-b9d4-61cb9e868869"},
+            {"SESSION-ID", "bf433055-79e1-437e-9ab9-378712326bc8"},
+            {"Content-Type", "application/json"}
+        },
+        cpr::Body{j.dump()}
+    );
+
+    nlohmann::json parsed = nlohmann::json::parse(res.text);
+
+    std::cout << res.status_code << '\n';
+
+    for(auto d: parsed["demand"])
     {
-        std::cout << it << '\n';
+        std::string customerId = d["customerId"].get<std::string>();
+        unsigned long long amount = d["amount"].get<unsigned long long>();
+        int postDay = d["postDay"].get<unsigned long long>();
+        int startDay = d["startDay"].get<unsigned long long>();
+        int endDay = d.at("endDay");
+
+        std::cout << customerId << ' ' << amount << ' ' <<  postDay << ' ' << startDay << ' ' << endDay << ' ' << std::endl;
     }
 }
-*/
+
--- API.h
+++ API.h
@@ -1,13 +1,16 @@
 #ifndef API_H
 #define API_H
 
+#include <unordered_map>
+#include <string>
+#include <vector>
 
 
 class API {
 public:
-    API();
-
-    //void startSession();
+    void startSession(std::unordered_map<std::string, std::vector<std::string>> data);
+    void endSession();
+    void playRound();
 };
 
 
